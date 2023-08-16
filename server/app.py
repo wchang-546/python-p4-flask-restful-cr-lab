af3d11dd33230626584f71bb7117c59e093b8e45
Will Chang <william.chaang@gmail.com>
2023-08-16 12:50:27 -0600

    REST create and retrieve practiced.

--- server/app.py
+++ server/app.py
@@ -17,11 +17,41 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [i.to_dict() for i in Plant.query.all()]
+        response = make_response(
+            response_dict_list,
+            200
+        )
+        return response
+    def post(self):
+        #This is new
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(
+            new_plant.to_dict(),
+            201
+        )
+        return response
+api.add_resource(Plants, "/plants")
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+            plant,
+            200
+        )
+        return response
+api.add_resource(PlantByID, "/plants/<int:id>")   
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
